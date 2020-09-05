b108e30376673b1ee0d79fd4f0e31523f781c577
Faber Vergara <fabervergara@gmail.com>
2020-09-05 11:38:17 -0500

    fix root path

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,19 +1,16 @@
 let mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 
-mix.js('src/js/app.js', 'dist/js')
+mix
+  .js('src/js/app.js', 'dist/js')
   .sass('src/sass/app.sass', 'dist/css')
   .options({
     processCssUrls: false,
-    postCss: [tailwindcss('./tailwind.config.js')]
+    postCss: [tailwindcss('./tailwind.config.js')],
   })
   .browserSync({
-    proxy: 'http://localhost/wp-test/',
+    proxy: 'http://localhost/vallunizate/',
     open: false,
-    files:[
-      'dist/css/app.css',
-      'dist/js/app.js',
-      './**/*.+(html|php)'
-    ]
+    files: ['dist/css/app.css', 'dist/js/app.js', './**/*.+(html|php)'],
   })
-  .sourceMaps(true, 'source-map')
+  .sourceMaps(true, 'source-map');
