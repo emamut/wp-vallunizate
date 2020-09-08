194fd25ac82955b3ab6896d6391f2a87f3042b28
Faber Vergara <fabervergara@gmail.com>
2020-09-08 11:00:20 -0500

    add rest api custom endpoint

--- helpers/rest_custom_endpoints.php
+++ helpers/rest_custom_endpoints.php
@@ -1,11 +1,26 @@
 <?php
-function custom_wp_menu() {
-  return wp_get_nav_menu_items(2);
-}
-
 add_action( 'rest_api_init', function () {
-  register_rest_route( 'wp/v2', 'menu', array(
+  register_rest_route( 'menu/v1/', 'get', array(
     'methods' => 'GET',
-    'callback' => 'custom_wp_menu',
-  ) );
-} );
+    'callback' => 'get_menu'
+  ));
+});
+
+function get_menu($request)
+{
+  $args = array(
+    'post_type' => 'menu',
+    'posts_per_page' => -1
+  );
+
+  $post_array = new WP_Query($args);
+  $post_array = $post_array->posts;
+
+  foreach ($post_array as $post)
+  {
+    $post->prize = get_field('precio', $post->ID);
+    $post->post_image = get_the_post_thumbnail_url($post->ID);
+  }
+
+  return $post_array;
+}
