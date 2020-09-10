3985f7b34196dfb63fa10de28b8cc92b2a520c7c
Faber Vergara <fabervergara@gmail.com>
2020-09-10 09:19:17 -0500

    add readme file and XML file with test data

---
+++ README.md
@@ -0,0 +1,13 @@
+# Vallunizáte
+
+Plantilla para Wordpress con [TailwindCSS](https://tailwindcss.com/), [VueJS](https://vuejs.org/) y [Laravel Mix](https://laravel-mix.com/) elaborado para la ponencia en **WordCamp Colombia Online 2020**
+
+## Instalación
+
+- Clona este repositorio en la carpeta `wp-content/themes`
+- Ejecuta el comando dentro de la carpeta clonada: `yarn` o `npm install`
+- Ejecuta `yarn watch` o `npm run watch` para ver tus cambios en `http://localhost:3000`
+- Instala los plugins necesarios
+- Si quieres el contenido de prueba, importa el archivo `vallunizte.WordPress.2020-09-10.xml` que se encuentra en la raíz de la carpeta clonada
+
+Puedes contactarme en Twitter como [@e_mamut](https://twitter.com/e_mamut)
--- dist/js/app.js
+++ dist/js/app.js
@@ -14955,15 +14955,14 @@ __webpack_require__.r(__webpack_exports__);
 /*!**********************************************!*\
   !*** ./src/js/components/OrderComponent.vue ***!
   \**********************************************/
-/*! no static exports found */
+/*! exports provided: default */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _OrderComponent_vue_vue_type_template_id_2b1e7d9b___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./OrderComponent.vue?vue&type=template&id=2b1e7d9b& */ "./src/js/components/OrderComponent.vue?vue&type=template&id=2b1e7d9b&");
 /* harmony import */ var _OrderComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./OrderComponent.vue?vue&type=script&lang=js& */ "./src/js/components/OrderComponent.vue?vue&type=script&lang=js&");
-/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _OrderComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__) if(["default"].indexOf(__WEBPACK_IMPORT_KEY__) < 0) (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _OrderComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__[key]; }) }(__WEBPACK_IMPORT_KEY__));
-/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
 
 
@@ -14993,7 +14992,7 @@ component.options.__file = "src/js/components/OrderComponent.vue"
 /*!***********************************************************************!*\
   !*** ./src/js/components/OrderComponent.vue?vue&type=script&lang=js& ***!
   \***********************************************************************/
-/*! no static exports found */
+/*! exports provided: default */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
